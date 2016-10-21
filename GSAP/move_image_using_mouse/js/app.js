fe3e186ec31399395ca73d7c65d9ca178cf2e63c
Anil Raj <anilraj659@gmail.com>
2016-10-21 15:33:12 +0530

    Simple React example on how to render a html tag using ReactDom.render

--- GSAP/move_image_using_mouse/js/app.js
+++ GSAP/move_image_using_mouse/js/app.js
@@ -6,12 +6,6 @@ function startTween(){
 }
 $(function() {
 $("body").mousemove(function(e) {
-
-  var offset = $(this).offset();
-  var relativeX = (e.pageX - offset.left);
-  var relativeY = (e.pageY - offset.top);
-
-  //alert("X: " + relativeX + "  Y: " + relativeY);
   TweenLite.to($("#toolbox"),1,{x:e.pageX-90,y:e.pageY-70});
 });
 });
