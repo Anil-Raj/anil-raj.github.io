aa1d36ef0c582a3b1ad967b6fba05ffa955728eb
Anil Raj <anilraj659@gmail.com>
2016-11-23 15:33:50 +0530

    resolved error

--- GSAP/move_image_using_mouse/js/app.js
+++ GSAP/move_image_using_mouse/js/app.js
@@ -5,10 +5,8 @@ function startTween(){
 	TweenLite.to($("#toolbox"),1,{x:250,y:250});
 }
 $(function() {
-<<<<<<< HEAD
 $("body").mousemove(function(e) {
   TweenLite.to($("#toolbox"),1,{x:e.pageX-90,y:e.pageY-70});
-=======
 $("body").touchmove(function(e) {
 
   var offset = $(this).offset();
@@ -17,7 +15,6 @@ $("body").touchmove(function(e) {
 
   //alert("X: " + relativeX + "  Y: " + relativeY);
   TweenLite.to($("#toolbox"),0,{x:e.pageX-90,y:e.pageY-70});
->>>>>>> 64af0b4c9e0a91dd67030b444ce352a4fe13141e
 });
 });
 //document.addEventListener("DOMContentLoaded",function(){
